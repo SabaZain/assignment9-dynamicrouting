92cf04106c016c1d721c649626a6ca8b6c67d78c
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2025-01-09 19:48:16 +0500

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <li className='text-green-700'>Phone No: 0334-3232678</li>
             <li className='text-green-700'>Email: saba_punjwani@yahoo.com</li>
         </ul>
-      <p className='text-center font-bold mt-2 p-1 bg-slate-800'>&copy; Copyright Data Fetching 2024 | All Rights Reserved</p>
+      <p className='text-center font-bold mt-2 p-1 bg-slate-800'>&copy; Copyright Data Fetching 2025 | All Rights Reserved</p>
     </div>
   )
 }
